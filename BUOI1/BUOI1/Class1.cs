532276de9e1000b1f0b658e9f50c7635eb2bafd9
phongdinhphan <93045210+phongdinhphan@users.noreply.github.com>
2021-10-24 14:50:23 +0700

    Add files via upload
    new code

---
+++ BUOI1/BUOI1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BUOI1
+{
+    class Class1
+    {
+    }
+}
---
+++ BUOI1/BUOI1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace BUOI1
+{
+    class Program
+    {
+        static float DTB(float a,float b)
+        {
+            float tb = (a + b) / 2;
+            return tb;
+        }
+        static int GiaiThua(int n)
+        {
+
+            int giaithua = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                giaithua *=i;
+                i++;
+            }
+            return giaithua;
+        }
+        static void Main(string[] args)
+        {
+            //cau1
+            float a=5;
+            float b=4;
+            float tb = (a + b) / 2;
+            Console.WriteLine("dtb:{0}", tb);
+            Console.ReadLine();
+            float TrungBinh;
+             TrungBinh = DTB(a,b);
+            if (tb < 5)
+            {
+                Console.WriteLine("rot");
+
+            }
+            else if (tb > 5 && tb < 8)
+            {
+                Console.WriteLine("Khá");
+            }
+            else
+            {
+                Console.WriteLine("giỏi");
+            }
+            //caau 2
+                Console.WriteLine("nhap vào giá trị a: ");
+                a = float.Parse(Console.ReadLine());
+                Console.WriteLine("nhap vào giá trị b: ");
+                b = float.Parse(Console.ReadLine());
+                Console.WriteLine("yeu cau nhap phep toan: ");
+                 char  pt = Console.ReadLine()[0];
+            switch (pt)
+            {
+                case '+':
+                    Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+                    break;
+                case '-':
+                    Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+                    break;
+                case '*':
+                    Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+                    break;
+                case '/':
+                    if (b <= 0)
+                    {
+                        Console.WriteLine("khong chia duoc");
+                    }
+                    else
+                        Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+                    break;
+
+            }
+            //bai3
+            int d;
+            Console.WriteLine("nhap so can tinh giathua: ");
+            d = int.Parse(Console.ReadLine());
+            int kqgt;
+            kqgt = GiaiThua(d);
+            Console.WriteLine("gia thua cua so vua nhap la {0}:", kqgt);
+            Console.ReadLine();
+
+
+
+
+
+        }
+
+        }
+    }
